11b7e590222ec8afcc20dd991f8ae240fd720d32
Cout <tesye30@hotmail.com>
2021-07-03 18:42:12 -0300

    1.3 implementação scroll, carousel, caracteres especiais, api whatsapp

--- main.js
+++ main.js
@@ -29,3 +29,30 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/* Testimonials carousel slider swiper*/
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* Scroll reveal: Mostrar elementos quando de scroll na pagina */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .text, #home .image
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials
+#contact .text, #contact .links`,
+  { interval: 100 }
+)
