32cf3863eb2fd0d271ed4d04b65c6ac6b045cf37
Cout <tesye30@hotmail.com>
2021-07-04 23:10:30 -0300

    scroll, midias social

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ for (const link of links) {
   })
 }
 /* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -28,7 +28,7 @@ window.addEventListener('scroll', function () {
     // menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testimonials carousel slider swiper*/
 const swiper = new Swiper('.swiper-container', {
@@ -53,6 +53,22 @@ scrollReveal.reveal(
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials
-#contact .text, #contact .links`,
+#contact .text, #contact .links,
+footer .brand, footer .social`,
   { interval: 100 }
 )
+
+/* Botao voltar para o topo */
+function backtotop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (this.window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backtotop()
+})
